10d738c8c81289cdba3e513891f9ec7ec2c85a29
vidacalura <lmguedes007@gmail.com>
2022-08-10 17:28:33 -0300

    Início do front-end

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'gray-100': '#434343',
+      'gray-200': '#2E2E2E',
+      'gray-300': '#282828',
+      'gray-400': '#1A1A1A',
+      'red': '#FF4545'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
