3c07d433d207a9a7ea8b8766891f7db4aae738de
vidacalura <lmguedes007@gmail.com>
2022-08-14 11:07:33 -0300

    Configuração do peerJS

--- public/JS/chat.js
+++ public/JS/chat.js
@@ -1,5 +1,6 @@
-// let socket = io();
+let socket = io();
 
+const videoGrid = document.getElementById("video-grid-1");
 const btnLigarCamera = document.getElementById("btn-camera");
 btnLigarCamera.addEventListener("click", alterarEstadoCamera);
 const btnLigarMicrofone = document.getElementById("btn-microfone");
@@ -19,6 +20,32 @@ const chatTextbox = document.getElementById("chat-textbox");
 const btnChatEnviarMsg = document.getElementById("btn-chat-env");
 btnChatEnviarMsg.addEventListener("click", enviarMensagem);
 const menuParticipantesDiv = document.getElementById("menu-participantes");
+
+const meuVideo = document.createElement("video");
+meuVideo.muted = true;
+
+const peers = { };
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+})
+.then((stream) => {
+    adicionarVideoStream(meuVideo, stream);
+
+    peer.on("call", (call) => {
+        call.answer(stream);
+
+        const video = document.createElement("video");
+        call.on("stream", (userVideoStream) => {
+            adicionarVideoStream(video, userVideoStream);
+        }); 
+    });
+
+    socket.on("usuarioConectado", (userId) => {
+        conectarNovoUsuario(userId, stream);
+    });
+});
 
 
 function alterarEstadoCamera(){
@@ -74,3 +101,55 @@ function enviarMensagem(){
 
 
 }
+
+function adicionarVideoStream(video, stream){
+
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+    });
+
+    const videoDiv = document.createElement("div");
+    videoDiv.classList.add("cam-div");
+    videoDiv.appendChild(video);
+
+    video.classList.add("object-cover");
+
+    videoGrid.appendChild(videoDiv);
+
+}
+
+function entrarSala(id){
+
+    socket.emit("entrarSala", { salaId, userId: id });
+
+}
+
+function conectarNovoUsuario(id, stream){
+
+    const call = peer.call(id, stream);
+    
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        adicionarVideoStream(video, userVideoStream);
+    });
+    call.on("close", () => {
+        video.parentElement.remove();
+    });
+
+    peers[id] = call;
+
+}
+
+/* PeerJS */
+const peer = new Peer(undefined, { host: "/", port: 3001 });
+
+peer.on("open", (id) => {
+    entrarSala(id);
+});
+
+/* Server listeners */
+socket.on("usuarioDesconectado", (data) => {
+    if (peers[data])
+        peers[data].close();
+});
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+const server = require("http").Server(app);
+
+const port = process.env.PORT || 5000;
+
+/* Express.js */
+app.set("view engine", "ejs");
+app.use(express.static("./public/"));
+
+app.get("/", (req, res) => {
+    // res.sendFile("index.html", { root: __dirname });
+});
+
+app.get("/:sala", (req, res) => {
+    res.render("chat", { salaId: req.params.sala });
+});
+
+
+const salas = {};
+let conexoes = 0;
+
+/* Socket.io */
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+
+    socket.on("entrarSala", (data) => {
+        const { salaId, userId } = data;
+
+        // verificação de existência da sala
+            socket.join(salaId);
+            socket.to(salaId).emit("usuarioConectado", userId);
+
+        socket.on("disconnect", () => {
+            socket.to(salaId).emit("usuarioDesconectado", userId);
+        });
+    });
+
+});
+
+server.listen(port);
+
+
+/* todo
+
+- Firebase
+- Permitir criação de salas na home
+- Criar dados de salas (objetos)
+- Bug tela preta
+- Fazer que cam é compartilhada apenas quando botão é apertado
+- Captcha para criar sala
+- limite de 250 conexões
+
+*/
